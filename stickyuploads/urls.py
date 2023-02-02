3712a21b4879070cbf1c158a22a8bac39d816772
JessiePBhalerao <jessie.bhalerao@gmail.com>
2023-02-02 12:44:39 -0600

    update for Django 4.1

--- stickyuploads/urls.py
+++ stickyuploads/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import UploadView
 
 urlpatterns = [
-    url(r'^default/$', UploadView.as_view(), name='sticky-upload-default'),
+    re_path(r'^default/$', UploadView.as_view(), name='sticky-upload-default'),
 ]
